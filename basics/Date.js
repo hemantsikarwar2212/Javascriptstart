2ba85d4b07fc0cc4463022f3e81b8d2ed34323a5
hemant sikarwar <94857718+hemantsikarwar2212@users.noreply.github.com>
2024-04-05 17:20:34 +0000

    commit config done

---
+++ basics/Date.js
@@ -0,0 +1,32 @@
+// let mydate = new Date()
+// console.log(mydate.toDateString());
+// console.log(mydate.toISOString());
+// console.log(mydate.toJSON());
+// console.log(mydate.toLocaleDateString());
+// console.log(mydate.toLocaleString());
+// console.log(mydate.toString());
+
+// // let myCreatedDate = new Date(2023 , 0 , 23 , 5 , 3)
+// // console.log(myCreatedDate.toDateString());
+// // console.log(myCreatedDate.toLocaleString());
+
+// let myCreatedDate = new Date ("01-14-2023")
+// console.log(myCreatedDate.toDateString());
+// console.log(myCreatedDate.toLocaleString());
+
+// let myTimeStamp = Date.now()
+// console.log(myTimeStamp);
+// console.log(myCreatedDate.getTime());
+// console.log(Date.now());
+// console.log(Math.floor(Date.now()/1000));
+
+ let newDate = new Date()
+ console.log(newDate.toDateString());
+// console.log(newDate.getMonth()+1);
+// console.log(newDate.getUTCMonth());
+
+
+
+
+
+
