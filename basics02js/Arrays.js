e048d4696cd2680349b0138691706b17a3dac74e
hemant sikarwar <94857718+hemantsikarwar2212@users.noreply.github.com>
2024-04-05 17:44:56 +0000

    first commit done

---
+++ basics02js/Arrays.js
@@ -0,0 +1,29 @@
+const arr  = [0,1,2,3,5]
+const myHeros = ["batman", "naruto" , "Luffy"]
+
+const arr2 = [5,1,6,7,5,85]
+console.log(myHeros[1]);
+
+// Arrays methods 
+// arr.push(8)
+// arr.push(9)
+// arr.pop()
+
+// arr.unshift(9)
+// arr.shift()
+// console.log(arr);
+// console.log(arr.includes(5));
+// console.log(arr.indexOf(5));
+// const newArray = arr.join()
+// console.log(newArray);
+
+
+/////////////SLICE////////
+console.log("a",arr);
+const newA1 = arr.slice(0,2)
+console.log(newA1);
+
+const newa2 = arr.splice(0,3)
+console.log(newa2);
+console.log(arr);
+
