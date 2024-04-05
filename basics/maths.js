6aa916ccd73f50d09537b305f855b990b350114d
hemant sikarwar <94857718+hemantsikarwar2212@users.noreply.github.com>
2024-04-05 16:57:10 +0000

    commit done

---
+++ basics/maths.js
@@ -0,0 +1,51 @@
+// const score = 400
+// console.log(score);
+
+
+// const balance = new Number(200)
+// console.log(balance);
+
+// console.log(balance.toString());
+// console.log(balance.toString().length);
+// console.log(balance.length);
+// console.log(balance.toFixed(2));
+
+// const othernumber = 452.8461;
+
+// console.log(othernumber.toPrecision(3));
+
+// const hundreds = 1000000
+// console.log(hundreds.toLocaleString('en-IN'));
+
+
+//+++++++++++++MATHS+++++++++++++++++++//
+// console.log(Math);
+// console.log(Math.abs(-3));
+// console.log(Math.round(5.64));
+// console.log(Math.ceil(54.57));
+// console.log(Math.floor(54.57));
+// console.log(Math.min(4,5,7,1,4,21,1,2));
+// console.log(Math.max(4,5,7,1,4,21,1,2));
+
+//Math.random always gives values b/w 0 and 1
+
+// console.log((Math.random()*10)+1);
+//when we need values in the interger form we use math.floor
+console.log(Math.floor(Math.random()*10)+1);
+
+const min = 10
+const max = 20 
+
+console.log(Math.floor(Math.random() * (max-min +1))+min)
+
+
+
+
+
+
+
+
+
+
+
+
