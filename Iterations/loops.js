40643564080b33cd5ce3482b6f0bef7e7950c5cf
hemant sikarwar <94857718+hemantsikarwar2212@users.noreply.github.com>
2024-04-12 18:15:52 +0000

    commit done

---
+++ Iterations/loops.js
@@ -0,0 +1,15 @@
+// FOR LOOP//
+// const array = [1,5,6,7,8,5,6]
+// for (let index = 0; index < array.length; index++) {
+//     const element = array[index];
+//     console.log(element);
+    
+// }
+for (let i = 0; i < 10; i++) {
+    console.log(`outer loop ${i}`);
+    for (let j = 0; j < array.length; j++) {
+        const element = array[j];
+        
+    }
+    
+}
